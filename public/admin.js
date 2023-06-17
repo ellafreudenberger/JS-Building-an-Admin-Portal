f795d85f6b87e721909a30fb9f6d8d48a2f740c2
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-06-16 23:06:10 -0700

    fetch requests

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,40 @@
+const { json } = require("body-parser");
+const { application } = require("express");
 
 // Your Code Here
+async function retrieve() {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+    
+    books.forEach(book => {
+        let booksList = document.getElementById('books'); 
+        
+        let listItem = document.createElement('li');
+        listItem.textContent = book.title;
+    
+        let listQuantity = document.createElement('input')
+        listQuantity.textContent = book.quantity;
+
+        let saveButton = document.createElement('button');
+        saveButton.textContent = 'Save';
+
+        saveButton.addEventListener('click', () => {
+            fetch('http://localhost:3001/updaetBook', {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: book.id,
+                    quantity: listQuantity.value
+                })
+            });
+        });
+    
+        listItem.append(listQuantity, saveButton);
+        
+        booksList.append(listItem);
+
+});
+}
+retrieve()
